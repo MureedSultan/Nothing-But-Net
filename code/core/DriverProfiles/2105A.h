d9ba6e7981f3fe379fd68d70a083bb8cf913bc00
Sultan <sultan.0313@gmail.com>
2015-09-02 17:13:07 -0400

    - Updated launcher speed ratios
    - Btn 7 ratios changed by a little bit

--- code/core/DriverProfiles/2105A.h
+++ code/core/DriverProfiles/2105A.h
@@ -5,25 +5,32 @@ task usercontrol {
 		motor[DriveFrontLeft] = vexRT[Ch4] + vexRT[Ch2] + vexRT[Ch1];
 		motor[DriveRearLeft] =  vexRT[Ch4] + vexRT[Ch2] - vexRT[Ch1];
 		if(vexRT[Btn7D] == 1){
-			motor[LeftA] = 127;
-			motor[LeftB] = 127;
-			motor[RightA] = 127;
-			motor[RightB] = 127;
+			if(nImmediateBatteryLevel/1000.0 > 8.4){
+				motor[LeftA] = 78;
+				motor[LeftB] = 78;
+				motor[RightA] = 78;
+				motor[RightB] = 78;
+				}else{
+				motor[LeftA] = 127;
+				motor[LeftB] = 127;
+				motor[RightA] = 127;
+				motor[RightB] = 127;
+			}
 			}else if(vexRT[Btn7R] == 1){
 			motor[LeftA] = 0;
 			motor[LeftB] = 0;
 			motor[RightA] = 0;
 			motor[RightB] = 0;
 			}else if(vexRT[Btn7L] == 1){
-			motor[LeftA] = 95;
-			motor[LeftB] = 95;
-			motor[RightA] = 95;
-			motor[RightB] = 95;
+			motor[LeftA] = 108;
+			motor[LeftB] = 108;
+			motor[RightA] = 108;
+			motor[RightB] = 108;
 			}else if(vexRT[Btn7U] == 1){
-			motor[LeftA] = 82;
-			motor[LeftB] = 82;
-			motor[RightA] = 82;
-			motor[RightB] = 82;
+			motor[LeftA] = 78;
+			motor[LeftB] = 78;
+			motor[RightA] = 78;
+			motor[RightB] = 78;
 		}
 		if(vexRT[Btn5U] == 1){
 			motor[CollectB] =  127;
